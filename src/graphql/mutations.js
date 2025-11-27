fd2a0d575a4a51fa0697ac8cb1fa58e3c57fe0e3
Mohamed Amine ELIDRISSI <130193461+Mohamedamine88@users.noreply.github.com>
2025-11-27 09:15:24 +0100

    Update mutations.js

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,5 @@
 import { gql } from '@apollo/client';
 
-// Mutation pour créer un compte
 export const SAVE_COMPTE = gql`
 mutation SaveCompte($compte: CompteRequest!) {
   saveCompte(compte: $compte) {
@@ -12,14 +11,12 @@ mutation SaveCompte($compte: CompteRequest!) {
 }
 `;
 
-// Mutation pour supprimer un compte
 export const DELETE_COMPTE = gql`
 mutation DeleteCompte($id: ID!) {
   deleteCompte(id: $id)
 }
 `;
 
-// Mutation pour ajouter une transaction
 export const ADD_TRANSACTION = gql`
 mutation AddTransaction($transactionRequest: TransactionRequest!) {
   addTransaction(transactionRequest: $transactionRequest) {
